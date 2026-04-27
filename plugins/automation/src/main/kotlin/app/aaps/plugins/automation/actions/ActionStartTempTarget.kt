cdf8d643156d232c6f1e92d49625634936b881d8
Der-Schubi <schubi@erlangen.ccc.de>
2026-04-27 07:42:15 +0200

    Allow Automation to set TT even if TT already exists

--- plugins/automation/src/main/kotlin/app/aaps/plugins/automation/actions/ActionStartTempTarget.kt
+++ plugins/automation/src/main/kotlin/app/aaps/plugins/automation/actions/ActionStartTempTarget.kt
@@ -20,8 +20,6 @@ import app.aaps.plugins.automation.R
 import app.aaps.plugins.automation.elements.ComparatorExists
 import app.aaps.plugins.automation.elements.InputDuration
 import app.aaps.plugins.automation.elements.InputTempTarget
-import app.aaps.plugins.automation.triggers.Trigger
-import app.aaps.plugins.automation.triggers.TriggerTempTarget
 import dagger.android.HasAndroidInjector
 import org.json.JSONObject
 import java.util.concurrent.TimeUnit
@@ -37,8 +35,6 @@ class ActionStartTempTarget(injector: HasAndroidInjector) : Action(injector) {
 
     var value = InputTempTarget(profileFunction)
     var duration = InputDuration(30, InputDuration.TimeUnit.MINUTES)
-
-    override var precondition: Trigger? = TriggerTempTarget(injector, ComparatorExists.Compare.NOT_EXISTS)
 
     override fun friendlyName(): Int = R.string.starttemptarget
     override fun shortDescription(): String = rh.gs(R.string.starttemptarget) + ": " + tt().friendlyDescription(value.units, rh, profileUtil)
